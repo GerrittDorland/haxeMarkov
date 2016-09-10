f149f3a992c444e3734a6a109386cd2dc37dd4db
Gerritt <gerrittdorland@gmail.com>
2016-09-10 02:05:32 -0700

    whatTHEFUCKISGOING ON

--- MainIntermediate.hx
+++ MainIntermediate.hx
@@ -7,15 +7,20 @@ import js.npm.express.*;
 import js.npm.Jade;
 import haxe.Timer;
 import de.polygonal.core.math.random.ParkMiller;
+import haxe.Utf8;
 class MainIntermediate
 {
+    var rng : ParkMiller;
+    var suffixMap : Map<String, Array<String> >;
+
     function new()
     {
         var app : Express   = new Express();
-        var rng : ParkMiller = new ParkMiller();
+        rng = new ParkMiller();
+        suffixMap = new Map();
 
         rng.setSeed(Std.int(Date.now().getTime()));
-
+        //sys.io.File.saveContent("fuggle", 'test.txt');
         app.set('port', 3000);
         app.set('view engine', 'jade');
         app.use(new Favicon(Node.__dirname + '/favicon.ico'));
@@ -64,5 +69,44 @@ class MainIntermediate
     static public function saveText(input:String)
     {
       trace(input);
+    }
+
+    static public function loadMap(myMap:Map<String, Array<String> > )
+    {
+      var fname = "database.txt";
+      var fin = sys.io.File.getContent(fname);
+      var subStrIndex : Int = 0;
+      var key1Delim : Utf8;
+      var key2Delim : Utf8;
+      var wordDelim : Utf8;
+      key1Delim = cast(0xff, Utf8);
+      key2Delim = cast(0xfe, Utf8);
+      wordDelim = cast(0xfd, Utf8);
+
+      while(subStrIndex <= fin.length - 5) //Just to be safe, I guess.
+      {
+        var subString = readLine(subStrIndex, fin);
+        var key1 = subString.split(cast(key1Delim, String));
+        var key2 = subString.split(cast(key2Delim, String));
+        var words = subString.split(cast(wordDelim, String));
+        myMap.set(key1[0] + key2[0], words);
+      }
+
+    }
+
+    static public function readLine(index:Int, data:String):String
+    {
+      var it : Int = index;
+      var itStart : Int = index;
+
+      while(data.charAt(it) != '\n' && data.charAt(it) != null)
+      {
+        it ++;
+        index ++;
+      }
+      //Don't increment index so we can skip the bullshit
+      it --;
+
+      return(data.substring(itStart, it));
     }
 }
